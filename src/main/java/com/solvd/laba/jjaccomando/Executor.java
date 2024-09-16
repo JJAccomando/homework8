7de0695044af1d988f93d6cce3203d3d2c99faf9
JJAccomando <jjaccomando.laba@solvd.com>
2024-09-16 11:20:38 -0700

    added TestFile.txt and method for finding unique words in file

--- src/main/java/com/solvd/laba/jjaccomando/Executor.java
+++ src/main/java/com/solvd/laba/jjaccomando/Executor.java
@@ -1,107 +1,65 @@
 package com.solvd.laba.jjaccomando;
 
-import java.util.Scanner;
 import com.solvd.laba.jjaccomando.enums.*;
 import com.solvd.laba.jjaccomando.exceptions.*;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.io.FileUtils;
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Executor {
 
+    public static void findUniqueWordsInFile() {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Please enter the file path: ");
+
+        try {
+            /* Creates a file object using the path given by the user input.
+            This input removes any enclosing quotations if the file path has them. */
+            File myFile = new File(StringUtils.strip(input.nextLine(), "\""));
+
+            /* The code below reads the contents of a file and returns the contents as one large String with all characters set to lower case
+            and then separates each word designated by the separator characters and then stores each word as an element in an array. */
+            String[] splitContent = StringUtils.split(FileUtils.readFileToString(myFile, "UTF-8").toLowerCase(), " \n\t\r.,;:!?()[]{}\"");
+
+            /* For each word in the splitContent array: add the word to the map as a key.
+            The default value is set to 0 for if the word is not in the map.
+            Everytime a word is added, the value that gets returned from that word as a key will increment by 1. */
+            Map<String, Integer> wordCount = new HashMap<>();
+            for (String word: splitContent) {
+                wordCount.put(word, wordCount.getOrDefault(word, 0) + 1);
+            }
+
+            /* For every word in the splitContent array, count the number of words that only appear once.
+            If the value returned is 1, then the word only appeared once. */
+            int uniqueWordCount = 0;
+            for (String word: splitContent) {
+                if (wordCount.getOrDefault(word, 0) == 1) {
+                    uniqueWordCount++;
+                }
+            }
+
+            /* Write to the file by adding a new line with the number of unique words. */
+            FileUtils.writeStringToFile(myFile, String.format("\nNumber of unique words: %d", uniqueWordCount), "UTF-8", true);
+
+            // Close Scanner
+            input.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static Logger logger = LogManager.getLogger();
+
     public static void main(String[] args) {
-        logger.info("Program start successful.");
-        Boeing737 plane = new Boeing737();
-        Flight flight = new Flight(plane, "LAX", "JFK");
-        Passenger person1 = new Passenger("John", "Smith");
-        Passenger person2 = new Passenger("George", "Washington");
-        SeatType seat = SeatType.ECONOMY_CLASS;
-
-        String firstName = "";
-        String lastName = "";
-        try (Scanner input = new Scanner(System.in)) { //Creates scanner within try-catch with resources block which will close scanner when finished
-            logger.warn("Receiving user input.");
-            System.out.print("Enter first name: ");
-            firstName = input.nextLine();
-            System.out.print("Enter last name: ");
-            lastName = input.nextLine();
-        } catch (Exception e) {
-            System.out.println("Something went wrong!");
-            logger.error("User input unsuccessful. Error");
-        }
-        logger.info("User input successful.");
-
-        Passenger person3 = new Passenger(firstName, lastName);
-        PassengerLuggage overweightBag = new PassengerLuggage(55);
-        logger.warn("Over weight luggage created.");
-        PassengerLuggage[] bags = new PassengerLuggage[15];
-        int weight = 45;
-        for (int i = 0; i < bags.length; i++) {
-            PassengerLuggage bag = new PassengerLuggage(weight);
-            bags[i] = bag;
-        }
-
-        try {
-            person3.addBags(overweightBag); //Try to add overweight PassengerLuggage to Passenger and throws OversizeBagException
-        } catch (OversizeBagException obe) {
-            System.out.println(obe.getMessage()); 
-            logger.error("Failed to add passenger luggage. Error: {OversizeBagException}");
-        } catch (OverLimitException ole) {
-            System.out.println(ole.getMessage());
-            logger.error("Failed to add passenger luggage. Error: {OverLimitException}");
-        }
-
-        System.out.println(person3.getNumBags()); //Displays 0 since no PassengerLuggage was added
-
-        try {
-            for (PassengerLuggage bag : bags) { //Try to add more than maximum allowed PassengerLuggage to Passenger and throws OverLimitException
-                person3.addBags(bag);
-            }
-        } catch (OversizeBagException obe) {
-            System.out.println(obe.getMessage());
-            logger.error("Failed to add passenger luggage. Error: {OversizeBagException}");
-        } catch (OverLimitException ole) {
-            System.out.println(ole.getMessage());
-            logger.error("Failed to add passenger luggage. Error: {OverLimitException}");
-        }
-
-        System.out.println(person3.getNumBags()); //Displays 10 because Passenger has maximum number of PassengerLuggage
-        try {
-            System.out.println(person3.getLuggage().toString());
-        } catch (EmptyBagException nbe) {
-            System.out.println(nbe.getMessage());
-            logger.error("Failed to retrieve passenger luggage information. Error: {EmptyBagException}");
-        }
-
-        try {
-            Passenger[] passengers = flight.getPassengers(); //Try to return Passenger array and throws EmptyPassengerException because no Passengers were added to Flight yet
-            for (Passenger passenger : passengers) { 
-                System.out.println(passenger); //Try to print each Passenger but will not execute because exception was thrown
-            }
-        } catch (EmptyPassengerException npe) {
-            System.out.println(npe.getMessage()); 
-            logger.error("Failed to retrieve passengers information. Error: {EmptyPassengerException}");
-        }
-
-        try {
-            flight.bookSeat(person1, seat); //Books Passenger on Flight
-            flight.bookSeat(person1, seat); //Try to book same Passenger on same Flight and throws DuplicateBookingException
-        } catch (DuplicateBookingException dbe) {
-            System.out.println(dbe.getMessage()); 
-            logger.error("Failed to add passenger to flight. Error: {DuplicateBookingException}");
-        }
-
-        System.out.println(flight.getNumPassengers()); //Prints out 1 because only one Passenger was added since second Passenger was a duplicate
-
-        try {
-            System.out.println(person1.getSeat()); //Prints person1's seat number
-            System.out.println(person2.getSeat()); //Try to print person2's seat number and throws EmptySeatException because person2 was never assigned a Seat
-        } catch (EmptySeatException nse) {
-            System.out.println(nse.getMessage()); 
-            logger.error("Failed to retrieve seat information. Error: {EmptySeatException}");
-        }
-
-        logger.info("Program success. Shutting down...");
+        findUniqueWordsInFile();
     }  
     
 }
